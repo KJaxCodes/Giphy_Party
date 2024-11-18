e3451c484a8de9dbc539353e86041ed1af3e9f35
Kelly J <[kelly4410@gmail.com]>
2024-11-18 08:53:55 -0700

    create new div for each gif

--- script.js
+++ script.js
@@ -14,50 +14,61 @@ document.addEventListener('DOMContentLoaded', function () {
     //     console.log(searchTerm);
     // })
 
-    function generateRandomNumber(multiplier {
-        return Math.floor(Math.random() * multipler);
+
+    //     function generateRandomNumber(multiplier {
+    //         console.log("Upper limit is " + multipler);
+    //         return Math.floor(Math.random() * multipler);
+    // });
+
+    let offset = 0;
+
+    gifButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        // console.log("I clicked the button");
+        const inputValue = searchInput.value;
+        giphyRequest(inputValue);
+    });
+
+    async function giphyRequest(searchTerm) {
+        console.log("Offset: " + offset);
+        const response = await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${searchTerm}&limit=50`)
+        console.log(response.data);
+        // const arrLength = response.data.data.length;
+        // const random = generateRandomNumber(arrLength);
+        // we run the random number generator five times;
+        // 5, 24, 31, 11, 3 
+        //Push all of the results to an array = [5, 24, 31, 11, 3]
+        //loop over that array
+        // displayDiv.innerHTML = ""; commenting out so that the gif isn't cleared when the button is clicked
+
+        let newDiv = document.createElement("div");
+        displayDiv.appendChild(newDiv);
+        console.log(newDiv);
+
+        let h4 = document.createElement("h4");
+
+        h4.innerHTML = response.data.data[offset].title;
+        newDiv.appendChild(h4);
+
+        let image = document.createElement("img");
+
+        image.src = response.data.data[offset].images.downsized.url;
+        image.alt = response.data.data[offset].alt_text;
+        image.id = "gif";
+        newDiv.appendChild(image);
+        offset += 1;
+    };
+
+    clearButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        console.log("Clear all");
+        console.log(displayDiv);
+        displayDiv.innerHTML = "";
+        searchInput.value = "";
+        //clear the input/search term?
+    });
 
 });
-
-
-gifButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    console.log("I clicked the button");
-    const inputValue = searchInput.value;
-    giphyRequest(inputValue);
-});
-
-async function giphyRequest(searchTerm) {
-    const response = await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${searchTerm}&limit=5`)
-    console.log(response.data);
-    const arrLength = response.data.data.length;
-    const random = generateRandomNumber(arrLength);
-
-    displayDiv.innerHTML = "";
-    let h4 = document.createElement("h4");
-    h4.innerHTML = response.data.data[random].title;
-    displayDiv.appendChild(h4);
-
-    const image = document.createElement("img");
-
-
-
-
-    image.src = response.data.data[random].images.downsized.url;
-    image.alt = response.data.data[random].alt_text;
-    image.id = "gif";
-    displayDiv.appendChild(image);
-};
-
-clearButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    console.log("Clear all");
-    console.log(displayDiv);
-    displayDiv.innerHTML = "";
-    searchInput.value = "";
-    //clear the input/search term?
-});
-
 
 // - set limit parameter - get the first 10... - got this
 // - offset/pagination - get the next 10... - look up pagination
@@ -74,7 +85,7 @@ clearButton.addEventListener("click", function (e) {
 //     console.log(searchTerm);
 //   }
 
-});
+
 
 
 //add next button to show the next set of GIFs?
